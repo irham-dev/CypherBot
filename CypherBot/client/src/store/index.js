2c8e4e8239b0e4df6c0729fc43f4479b856794a9
Shiningsolstice <futuhirhamain18@gmail.com>
2026-06-25 21:32:45 +0700

    Delete CypherBot directory

--- CypherBot/client/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import messageReducer from './messageSlice';
-
-export const store = configureStore({
-  reducer: {
-    messages: messageReducer
-  }
-});
